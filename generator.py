ff51952b8ad57632e48211035f176d77ba98f290
Nikita <28987828+anykeymeister@users.noreply.github.com>
2025-08-29 08:40:17 +0300

    Update generator.py
    removed unnecessary variables

--- generator.py
+++ generator.py
@@ -1,4 +1,3 @@
-mCryptonInt = 514365010
 chip_id_input = input("Please enter chipID: ")
 
 def encrypt_string(chip_id):
@@ -6,26 +5,15 @@ def encrypt_string(chip_id):
         return ""
 
     try:
-        #print(f"encrypt deviceID from setting: {chip_id}")
-
         if len(chip_id) < 8:
             return chip_id
 
-        substring = chip_id[-8:]
-        numeric_value = int(substring, 16) + mCryptonInt
-        encrypted_value = numeric_value ^ 174714
-        result = hex(encrypted_value)[2:]  # Convert to hex and remove '0x' prefix
+        result = (hex((int(chip_id[-8:], 16) + 514365010) ^ 174714)[2:])[-6:]
 
-        if len(result) > 6:
-            result = result[-6:]
-
-        #print(f"encrypt result: {result}")
         return result
 
     except Exception as e:
         print(f"encrypt error: {e}")
         return ""
 
-
-encrypted = encrypt_string(chip_id_input)
-print(f"Anykeymeister made this code for you: {encrypted}")
+print(f"Anykeymeister made this code for you: {encrypt_string(chip_id_input)}")
